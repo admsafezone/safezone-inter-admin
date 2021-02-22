86395238e6fad370a768c580252b659c8ad0196b
Eliton Freitas <elitonfreitas@gmail.com>
2021-02-22 20:11:30 -0300

    refactor: company token id to _id

--- src/providers/AppProvider/index.tsx
+++ src/providers/AppProvider/index.tsx
@@ -61,7 +61,7 @@ export const AppProvider = ({ children }: any) => {
 
   const getCompany = async () => {
     const response = await defaultService.get(
-      `${Constants.api.COMPANIES}/${user?.company?.id}?select=name theme identifier`,
+      `${Constants.api.COMPANIES}/${user?.company?._id}?select=name theme identifier`,
     );
     sls.setItem(Constants.storage.COMPANY, response);
     setCompany(response);
