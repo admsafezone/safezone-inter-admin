559f4c4932f98551ace8f84fb7150a68fe0f7820
Eliton Freitas <elitonfreitas@gmail.com>
2021-07-04 17:29:00 -0300

    refactor: card total loading

--- src/components/atoms/OneCard/index.tsx
+++ src/components/atoms/OneCard/index.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactElement } from 'react';
 import { Card } from 'antd';
+import { useAppContext } from 'providers/AppProvider';
 import './style.less';
 
 interface Results {
@@ -15,11 +16,12 @@ interface Props {
 }
 
 const OneCard: FC<Props> = (props: Props): ReactElement => {
+  const { t } = useAppContext();
   return (
     <Card className={`dashboard-card`}>
       <div className={`dashboard-card-icon ${props.className}`}>{props.children}</div>
       <div className="dashboard-card-info">
-        <h3>{props?.title}</h3>
+        <h3>{props?.title || `${t('Loading')}...`}</h3>
       </div>
       <div className="dashboard-card-value">{props?.results?.total || 0}</div>
     </Card>
